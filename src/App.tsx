1b926e3d976756cc2a61b28133df9d6ac860ecbd
Luciana Kyoko <lukyoko@protonmail.com>
2025-05-01 17:05:48 -0300

    feat(components): create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Navbar } from "./components/Navbar"
+
+
 function App() {
-  <div>
-    app
+  <div className="w-full bg-[#FAF4E3]">
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/"/>
+        <Route path="/sobre"/>
+        <Route path="/contato"/>
+        <Route path="/animais"/>
+        <Route path="/animais/:id"/>
+
+        {/* ----- Proteger esta rota: ----- */}
+        <Route path="/dashboard"/>
+      </Routes>
+    </BrowserRouter>
   </div>
 }
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <nav className="w-full py-6 md:px-16 ">
+      {/* ----- Mobile & Tablet ----- */}
+      <div className="flex flex-col items-center gap-3 lg:hidden mb-4 px-4">
+        <div className="flex items-center justify-between w-full">
+          <Link to="/">
+            <img 
+              src="/logo.png" 
+              alt="Logo Lar São Francisco" 
+              className="w-[80px]"
+            />
+          </Link>
+          <div className="flex gap-2 items-center">
+            <Link to="/dashboard" className="font-bold text-[#2e2d2d] hover:text-[#121417]">
+              Dashboard
+            </Link>
+            <Link 
+              to="/login" 
+              className="bg-blue-500 text-white py-1 px-4 rounded-full hover:bg-blue-600 transition"
+            >
+              Entrar
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link to="/sobre" className="font-bold text-[#2e2d2d] hover:text-[#121417]">Sobre</Link>
+          <Link to="/animais" className="font-bold text-[#2e2d2d] hover:text-[#121417]">Adote/ Doe</Link>
+          <Link to="/contato" className="font-bold text-[#2e2d2d] hover:text-[#121417]">Contato</Link>
+        </div>
+      </div>
+
+      {/* ----- Laptop & Desktop ----- */}
+      <div className="hidden lg:flex flex-row items-center justify-between w-full">
+        <Link to="/">
+          <img 
+            src="/logo.png" 
+            alt="Logo Lar São Francisco" 
+            className="w-[80px]"
+          />
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Link to="/sobre" className="font-bold text-[#2e2d2d] hover:text-[#121417]">Sobre</Link>
+          <Link to="/animais" className="font-bold text-[#2e2d2d] hover:text-[#121417]">Adote/ Doe</Link>
+          <Link to="/contato" className="font-bold text-[#2e2d2d] hover:text-[#121417]">Contato</Link>
+        </div>
+
+        {/* Botão de login */}
+        <div className="flex gap-2 items-center">
+          <Link to="/dashboard" className="font-bold text-[#2e2d2d] hover:text-[#121417]">
+            Dashboard
+          </Link>
+
+          <Link 
+            to="/login" 
+            className="bg-blue-500 text-white py-1 px-4 rounded-full hover:bg-blue-600 transition"
+          >
+            Entrar
+          </Link>
+
+          <button 
+            className="bg-blue-500 text-white py-1 px-4 rounded-full hover:bg-blue-600 transition"
+          >
+            Sair
+          </button> 
+        </div>
+      </div>
+    </nav>
+  );
+}
+
